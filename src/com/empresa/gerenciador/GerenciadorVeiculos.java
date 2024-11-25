f6ba44c11c3fd723af25578cf0785966fc8b5cf5
Wytallo Vinicius <wytalloproject@gmail.com>
2024-11-25 12:01:10 -0300

    Adicionando apenas as pastas do projeto

---
+++ src/com/empresa/gerenciador/GerenciadorVeiculos.java
@@ -0,0 +1,10 @@
+package com.empresa.gerenciador;
+
+public class GerenciadorVeiculos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/empresa/gerenciador/package-info.java
@@ -0,0 +1,1 @@
+package com.empresa.gerenciador;
